fbe20baaf49510c1927adbf480f385d4328d1da7
emorymatthew <46791077+emorymatthew@users.noreply.github.com>
2019-01-17 11:26:33 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+1 /**
+2 * Author: Matthew Emory
+3 * Date: 2019/01/17
+4 *
+5 * A simple hello world program in C
+6 *
+7 */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+int main(int argc, char **argv) {
+
+ printf("Matthew Emory\n");
+
+ return 0;
+ }
